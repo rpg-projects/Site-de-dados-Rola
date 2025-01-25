7c6af901447f5716fe0a76b1cb12bf18c0690288
ElieneMontenegro <lila.amaral@hotmail.com>
2025-01-25 01:07:57 -0300

    sala de mensagens!

--- frontend/src/components/logo-box/logoBox.tsx
+++ frontend/src/components/logo-box/logoBox.tsx
@@ -17,6 +17,7 @@ import {
   StyledInput,
 } from "../commons";
 import logo from "../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 import "./logoBox.css";
 
@@ -29,8 +30,18 @@ function LogoBox({
   style?: React.CSSProperties;
   className?: string;
 }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/"); // Navigate to the home page
+  };
+
   return (
-    <div className={`box ${className || ""}`} style={style}>
+    <div
+      className={`box ${className || ""}`}
+      style={style}
+      onClick={handleClick}
+    >
       <p className="nome">ROLA</p>
       <img className="img" src={logo} alt="Logo" />
     </div>
